fc452d6216b679504d7228115990d41a51b5b205
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2022-06-10 14:18:23 +0200

    preparation of the work environment

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import VideoApp from './VideoApp';
+
+const App = () => <VideoApp />;
+
+export default App;
---
+++ src/components/VideoApp.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const VideoApp = () => <h1> TEST </h1>;
+
+export default VideoApp;
